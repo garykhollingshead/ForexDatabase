1075fd3a85f6911550a01aecf84eee307aa1f969
gkhollingshead <gary.k.hollingshead@gmail.com>
2017-01-31 19:16:01 -0800

    add sproc

---
+++ Auth/dbo/Stored Procedures/GetClientCredential.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE GetClientCredential @ClientName NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT ClientName,
+	ClientSecret
+	FROM Auth.dbo.SignInCredentials
+	WHERE @ClientName = ClientName
+END
---
+++ Auth/dbo/Tables/SignInCredentials.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SignInCredentials] (
+    [Id]           INT            NOT NULL,
+    [ClientName]   NVARCHAR (50)  NOT NULL,
+    [ClientSecret] NVARCHAR (200) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
